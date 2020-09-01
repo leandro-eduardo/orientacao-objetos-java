cf3c059a2d38d81c37cc4130c4f419ae862b6b99
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2020-09-01 12:29:06 -0300

    Adicionados primeiros exercícios (aula 2.10)

---
+++ fundamentos-java/Variavel1.java
@@ -0,0 +1,30 @@
+public class Variavel1 {
+
+	public static void main(String[] args) {
+		int quantidade_alunos;
+		int QUANTIDADE;
+		int QuantidadeAlunos;
+		int $quantidade;
+		int _quantidade;
+		int quantidade_alunos_nota_10;
+			
+	    //int 2alunos; // não pode começar com números
+		//int quantidade alunos; // não pode ter espaços
+		//int new; // não pode ser uma palavra reservada
+		// não é uma boa prática abreviar demais como nos exemplos abaixo
+		int qtAlu; 
+		int quantAlunN10;
+		
+		// seguindo a Code Convetion (Convenção de Código)
+		int quantidade; // se o nome da variável possuir apenas uma palavra: todas as letras minúsculas
+		int quantidadeAlunos; // se o nome da variável possuir mais de uma palavra: primeira palavra toda minúscula e as inicias das demais palavaras em maiúsculo
+		int quantidadeAlunosNota10;
+		int numeroDeAlunosAprovados;
+		
+		quantidade = 10;
+		System.out.println(quantidade);
+		quantidade = 15;
+		System.out.println(quantidade);	
+	}
+	
+}
