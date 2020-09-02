7604d98771cd651bdc15d8e691683f611f1826a3
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2020-09-02 13:15:29 -0300

    Adicionados novos exercícios (2.14)

---
+++ fundamentos-java/ConversaoTipoPrimitivo.java
@@ -0,0 +1,28 @@
+public class ConversaoTipoPrimitivo {
+
+	public static void main(String[] args) {
+		long x = 9300000035l;
+		// casting
+		int y = (int) x;
+		System.out.println(y);
+		
+		int c = 102344;
+		long d = c;
+		System.out.println(d);
+		
+		double a = 20.5;
+		// casting
+		float b = (float) a;
+		System.out.println(b);
+		
+		float e = 934.5f;
+		double f = c;
+		System.out.println(e);
+		
+		double largura = 100.37;
+		// casting
+		int tamanho = (int) largura;
+		System.out.println(tamanho);
+	}	
+	
+}	
---
+++ fundamentos-java/PromocaoAritimetica.java
@@ -0,0 +1,21 @@
+public class PromocaoAritimetica {
+
+	public static void main(String[] args) {
+		/*int x = 10;
+		long y = 5;
+		long z = x + y;
+		System.out.println(z);*/
+		
+		/*long x = 10;
+		float y = 9.34f;
+		float z = x * y;
+		System.out.println(z);*/
+		
+		int x = 3;
+		int y = 2;
+		//casting
+		float z = (float) x / y;
+		System.out.println(z);
+	}	
+	
+}	
