b575b512c53fa61db913a3aa8d04e292a9e79b2c
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2020-09-24 11:41:15 -0300

    Adicionados novos conteúdos: modificadores static e final (5.11)

---
+++ desafio-static-final/src/MatematicaUtil.java
@@ -0,0 +1,17 @@
+
+public class MatematicaUtil {
+	
+	public static int calcularFibonacci(int posicao) {
+		if (posicao < 2) {
+			return posicao;
+		}
+		return calcularFibonacci(posicao - 1) + calcularFibonacci(posicao - 2);
+	}
+	
+	public static final double PI = 3.14;
+	
+	public static double calcularAreaCirculo(double raio) {
+		return (raio * raio) * PI;
+	}
+	
+}
