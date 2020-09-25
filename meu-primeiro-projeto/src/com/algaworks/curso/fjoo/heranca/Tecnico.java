ae12529d8ba98157fbd3d3ea499b11eb9ef6cf0a
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2020-09-25 11:09:55 -0300

    Adicionados novos conteúdos: herança, modificador protected e classe object (5.15)

---
+++ meu-primeiro-projeto/src/com/algaworks/curso/fjoo/heranca/Tecnico.java
@@ -0,0 +1,6 @@
+package com.algaworks.curso.fjoo.heranca;
+
+public class Tecnico extends Pessoa {
+
+}
+ 
---
+++ meu-primeiro-projeto/src/com/algaworks/curso/fjoo/heranca/TesteHeranca.java
@@ -0,0 +1,14 @@
+package com.algaworks.curso.fjoo.heranca;
+
+public class TesteHeranca {
+
+	public static void main(String[] args) {
+		Jogador j = new Jogador();
+		j.nome = "Ronaldo";
+		j.idade = 44;
+		j.seApresentar();
+		j.dizerSeAindaJoga();
+				
+	}
+
+}
