2aaa41cfda3dba2f0252d505911d5ceff0e857b0
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2020-09-28 12:13:13 -0300

    Adicionados novos conteúdos: polimorfismo, instanceof e casting de objetos (5.20)

---
+++ meu-primeiro-projeto/src/com/algaworks/curso/fjoo/polimorfismo/Conta.java
@@ -0,0 +1,17 @@
+package com.algaworks.curso.fjoo.polimorfismo;
+
+public class Conta {
+
+	protected double saldo;
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
+}
